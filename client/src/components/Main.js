75a5cc38870a2d70ef3545178aae8ce799887d62
setare mehr <setaremehr89@gmail.com>
2020-11-25 00:27:54 -0800

    getting response from api

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,22 +1,45 @@
 import { Component } from "react";
-import API from "../utils/API";
+// import API from "../utils/API";
+// import Info from "./Info";
 // import axios from "axios";
 
 class Main extends Component {
-  state = {
-   users: [],
-    search: "",
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
 
+    }
+  }
   componentDidMount() {
-    API.getAllUsers().then(response => this.setState({
-        users: response.data.results
-    })).catch(e => console.log(e))
+
+    fetch("https://randomuser.me/api/")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          items: res.results,
+        })
+      })
+    // API.getAllUsers()
+    // .then(res => {
+    //   this.setState({
+    //     items: res.results,
+    //     // filteredUsers: results.data.results
+    //   });
+    // });
   }
   render() {
+    var { items } = this.state
     return <div>
-       
-    
+      {/* <Info users={this.state.users} /> */}
+      {items.map(item => (
+        <div>
+          <img src={item.picture.medium} alt="" />
+          <p>{item.name.first} {item.name.last}</p>
+          <p>{item.email}</p>
+          <p>{item.phone}</p>
+        </div>
+      ))}
     </div>;
   }
 }
